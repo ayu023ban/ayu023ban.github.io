8edde302a23ee96397b0fc0c3075b752a6f04195
Ayush Bansal <ayu023ban@gmail.com>
2021-03-18 09:12:07 +0530

    Updates

---
+++ precache-manifest.c7aa2881701fbbf4a24cc7f69c572cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e7fb1cfbf73c04c37b520ba8d4c8e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a0f8a5d4b1771096b73",
+    "url": "/static/css/main.1999253e.chunk.css"
+  },
+  {
+    "revision": "5f4947fa6d2e17f6d8a2",
+    "url": "/static/js/2.341bc894.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.341bc894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0f8a5d4b1771096b73",
+    "url": "/static/js/main.ee2096e8.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "29c4f191aa1360f426dc4c035f4df718",
+    "url": "/static/media/Resume.29c4f191.pdf"
+  }
+]);
