88101915d5878166e92c50eacca45f0b745a059c
ayu023ban <ayu023ban@gmail.com>
2021-06-06 12:43:38 +0000

    Deploying to gh-pages from  @ 0e7c881d146180f0242fbfcca1e9b7c0130b5c7e 🚀

---
+++ precache-manifest.27f80a6b749bc0bc83a146eb5083f7d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7671fc6b09f6bd5932e",
+    "url": "/static/css/main.4cd13fb9.chunk.css"
+  },
+  {
+    "revision": "a7671fc6b09f6bd5932e",
+    "url": "/static/js/main.8410179f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3787dd3fc8347f75d86a",
+    "url": "/static/js/2.8d863029.chunk.js"
+  },
+  {
+    "revision": "43fc43350612b8957714581d8b098258",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec41745d338090fae0f9eca481b89c2c.js"
+  "/precache-manifest.27f80a6b749bc0bc83a146eb5083f7d1.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
