4003bc0cfb1d8dbd57f4ce1c51250b79a3116575
Ayush Bansal <ayu023ban@gmail.com>
2021-06-06 11:46:26 +0530

    Update resume
    Add GSoC details
    
    Add functionality of adding future endings of jobs
    
    Signed-off-by: Ayush Bansal <ayu023ban@gmail.com>

--- src/components/UI/MasterDetail/Detail/Detail.js
+++ src/components/UI/MasterDetail/Detail/Detail.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import classes from './Detail.module.css';
+import React from "react";
+import classes from "./Detail.module.css";
 
 const Detail = (props) => {
   const job = props.job;
-
+  const job_end = new Date(job.till);
   return (
     <div className={classes.Detail}>
       <div className={classes.Heading}>
-        {job.jobTitle} @{' '}
+        {job.jobTitle} @{" "}
         <a
           href={job.link}
-          target='_blank'
-          rel='noopener noreferrer'
+          target="_blank"
+          rel="noopener noreferrer"
           className={classes.CompanyName}
         >
           {job.company}
         </a>
       </div>
       <div className={classes.Time}>
-        {job.from} - {job.till}
+        {job.from} - {job_end.getTime() > Date.now() ? "present" : job.till}
       </div>
       <div
         dangerouslySetInnerHTML={{ __html: job.description }}
